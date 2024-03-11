f4b8649fd597f39dc9723e17cfd233b6c3df17fb
Luiz <lucachaves@gmail.com>
2024-03-10 22:09:29 -0300

    definições do projeto e de dados da disciplina

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,33 @@
+// subject data
+export const SUBJECT = {
+  CLASSROOM_URLS: {
+    A: 'https://classroom.google.com/c/NjYzNjMwNzIzNzg1',
+  },
+  CHAT_URL: 'https://mail.google.com/mail/u/0/#chat/space/AAAAAji4jww',
+  PROJECTS_URL: 'https://padlet.com/lucachaves/projetodw20241',
+  GITHUB_URLS: {
+    A: 'https://github.com/lucachaves/dw-2024-1',
+  },
+  SCHEDULES: [
+    {
+      room: 'Lab 58',
+      day: 'Segunda',
+      start: '07:00',
+      end: '08:40',
+    },
+    {
+      room: 'Lab 58',
+      day: 'Quarta',
+      start: '07:00',
+      end: '10:40',
+    },
+  ],
+};
+
+// project data
 export const GITHUB_EDIT_URL = 'https://github.com/ifpb/dw/tree/main';
 
-export const CLASSNOTE_SLUGS = [
+export const CONTENT_SLUGS = [
   'markup/languages',
 
   'html/introduction',
@@ -44,6 +71,8 @@ export const CLASSNOTE_SLUGS = [
   'expressjs/auth',
   // docker
   // test
+  // ts
+  // documentation
 
   'extra/next-steps',
   'extra/exercises',
