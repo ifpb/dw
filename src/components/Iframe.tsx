acbb42436b3f2ea62dca9b76ec095e9071dbf2da
Luiz Chaves <lucachaves@gmail.com>
2024-10-23 13:59:02 -0300

    fix: improve dark mode in iframe

--- src/components/Iframe.tsx
+++ src/components/Iframe.tsx
@@ -14,60 +14,68 @@ export default function Iframe({ src, srcDoc, height, showDarkMode }: Props) {
 
   const iFrameRef = useRef<HTMLIFrameElement>(null);
 
-  const $isDark = showDarkMode || useStore(isDark);
+  const $isDark = showDarkMode ?? useStore(isDark);
 
-  iFrameRef?.current?.contentWindow?.document.head.insertAdjacentHTML(
-    'beforeend',
-    `<style>
-      :root {
-        --color-primary: #000;
-      }
+  // if not exist style#dark-mode-style, insert it
+  if (
+    $isDark &&
+    !iFrameRef?.current?.contentWindow?.document.head.querySelector(
+      '#dark-mode-style'
+    )
+  ) {
+    iFrameRef?.current?.contentWindow?.document.head.insertAdjacentHTML(
+      'beforeend',
+      `<style id="dark-mode-style">
+        :root {
+          --color-primary: #000;
+        }
 
-      :root.dark {
-        --color-primary: #fff;
-      }
+        :root.dark {
+          --color-primary: #fff;
+        }
 
-      body {
-        color: var(--color-primary);
-      }
+        body {
+          color: var(--color-primary);
+        }
 
-      a {
-        color: var(--color-primary);
-      }
+        a {
+          color: var(--color-primary);
+        }
 
-      * {
-        border-color: var(--color-primary);
-      }
+        * {
+          border-color: var(--color-primary);
+        }
 
-      /* * {
-         border-color: var(--color-primary) !important;
-       } */
+        /* * {
+           border-color: var(--color-primary) !important;
+         } */
 
-       ::-webkit-scrollbar {
-        width: 10px;
-        height: 10px;
-      }
+         ::-webkit-scrollbar {
+          width: 10px;
+          height: 10px;
+        }
 
-      ::-webkit-scrollbar-button:start:decrement,
-      ::-webkit-scrollbar-button:end:increment {
-        display: none;
-      }
+        ::-webkit-scrollbar-button:start:decrement,
+        ::-webkit-scrollbar-button:end:increment {
+          display: none;
+        }
 
-      *::-webkit-scrollbar-track-piece {
-        @apply bg-transparent;
-        -webkit-border-radius: 6px;
-      }
+        *::-webkit-scrollbar-track-piece {
+          @apply bg-transparent;
+          -webkit-border-radius: 6px;
+        }
 
-      *::-webkit-scrollbar-thumb:vertical {
-        @apply bg-gray-400;
-        -webkit-border-radius: 6px;
-      }
+        *::-webkit-scrollbar-thumb:vertical {
+          @apply bg-gray-400;
+          -webkit-border-radius: 6px;
+        }
 
-      * {
-        scrollbar-color: #9ca3af transparent;
-      }
-    </style>`
-  );
+        * {
+          scrollbar-color: #9ca3af transparent;
+        }
+      </style>`
+    );
+  }
 
   if ($isDark) {
     iFrameRef?.current?.contentWindow?.document.documentElement.classList.add(
